c7ab99242d2562d0dfe423bace8ac11c5ba0aab9
相澤聡 <aizawasatoshi03@gmail.com>
2016-07-07 16:24:36 +0900

    a

--- node/server.js
+++ node/server.js
@@ -1,9 +1,49 @@
-var http = require("http");
+//var http = require("http");
+//
+//http.createServer(function (request, response) {
+//    response.writeHead(200, {
+//        "Content-Type": "text/plain"
+//    });
+//    response.write("Hello World");
+//    response.end();
+//}).listen(8888);
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+//var http = require("http");
+//
+//function start() {
+//    function onRequest(request, response) {
+//        console.log("Request received.");
+//        response.writeHead(200, {
+//            "Context-Type": "text/plain"
+//        });
+//        response.write("Hello World");
+//        response.end();
+//    }
+//
+//    http.createServer(onRequest).listen(8888);
+//    console.log("Server has started");
+//}
+//
+//exports.start = start;
+
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + "received.");
+
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        var content = route(handle, pathname);
+        response.write(content);
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
