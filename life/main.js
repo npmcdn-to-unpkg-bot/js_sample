628956c7bcdfc589a0bd7b3e9e0fc6242d1d2389
相澤聡 <aizawasatoshi03@gmail.com>
2016-07-10 16:04:26 +0900

    a

--- life/main.js
+++ life/main.js
@@ -2,7 +2,7 @@
 var SCREEN_SIZE = 500;
 var SIDE_CELLS = 200;
 var CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;
-var FPS = 100;
+var FPS = 10000;
 var canvas;
 var context;
 
@@ -11,7 +11,7 @@ window.onload = function () {
     var field = new Array(SIDE_CELLS * SIDE_CELLS);
     var tempField = new Array(SIDE_CELLS * SIDE_CELLS);
     for (var i = 0; i < field.length; i++) {
-        field[i] = Math.floor(Math.random(): 2);
+        field[i] = Math.floor(Math.random() * 2);
     }
     canvas = document.getElementById('world');
     canvas.width = canvas.height = SCREEN_SIZE;
@@ -30,11 +30,11 @@ function update(field, tempField) {
     tempField = field.slice();
     for (var i = 0; i < tempField.length; i++) {
         n = 0;
-        for (var s -= 1; s < 2; s++) {
-            for (var t -= 1; t < 2; t++) {
+        for (var s = -1; s < 2; s++) {
+            for (var t = -1; t < 2; t++) {
                 if (s == 0 && t == 0) continue;
                 var c = i + s * SIDE_CELLS + t;
-                if (c >= 0 && c < % SIDE_CELLS != 0 || i > c && c % SIDE_CELLS != SIDE_CELLS - 1) {
+                if (c >= 0 && c % SIDE_CELLS != 0 || i > c && c % SIDE_CELLS != SIDE_CELLS - 1) {
                     if (i < c && c % SIDE_CELLS != 0 || i > c && c % SIDE_CELLS != SIDE_CELLS - 1) {
                         if (tempField[c]) n++;
                     }
@@ -42,7 +42,7 @@ function update(field, tempField) {
             }
         }
         if (tempField[i] && (n == 2 || n == 3)) {
-            field[i] 1;
+            field[i] = 1;
         } else if (!tempField[i] && n == 3) {
             field[i] = 1;
         } else field[i] = 0;
@@ -51,8 +51,14 @@ function update(field, tempField) {
     setTimeout(update, 1000 / FPS, field, tempField); //ミリ秒後に再起処理をしてループ
 }
 
-function draw() {
+function draw(field) {
     /* キャンパスの描画処理。update関数で呼び出される予定 */
+    context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);
+    for (var i = 0; i < field.length; i++) {
+        var x = (i % SIDE_CELLS) * CELL_SIZE;
+        var y = Math.floor(i / SIDE_CELLS) * CELL_SIZE;
+        if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+    }
 
 
 }
