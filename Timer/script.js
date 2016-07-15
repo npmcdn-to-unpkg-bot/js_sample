9b380a5ce5a5a89e5813086d07edfe47efdadff4
相澤聡 <aizawasatoshi03@gmail.com>
2016-07-15 21:32:33 +0900

    a

--- Timer/script.js
+++ Timer/script.js
@@ -3,24 +3,39 @@
 
     // window.onloadし忘れた
     var time = document.getElementById('time');
-    var min = document.getElementById('min');
+    //    var min = document.getElementById('min');
     var start = document.getElementById('start');
-    var stop = document.getElementById('stop');
-    var reset = document.getElementById('reset');
+    //    var stop = document.getElementById('stop');
+    //    var reset = document.getElementById('reset');
+    var rest = 180;
+    var hello = "hello";
+    console.log('hello');
+
+
+    console.log(rest);
 
     start.addEventListener('click', function () {
         // time.innerHTML = min.value;
-        var startTime = Date.now();
-        var id = setInterval({
 
+        var id = setInterval(function () {
+            rest--;
+            time.innerHTML = rest;
+            if (rest <= 0) {
+
+                clearInterval(id);
+                alert('終了');
+            }
         }, 1000);
     });
 
-    stop.addEventListener('click', function () {
-        clearInterval(id);
-    });
 
-    reset.addEventListener('click', function () {
-        time.innerHTML = '00:00';
-    });
+
+
+    //    stop.addEventListener('click', function () {
+    //        clearInterval(id);
+    //    });
+    //
+    //    reset.addEventListener('click', function () {
+    //        time.innerHTML = '00:00';
+    //    });
 })();
